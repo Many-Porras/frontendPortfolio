72cad3a97025ef5d517b430a91db8b1ddcb63eb1
Many Richard Porras <r.porras.avila@gmail.com>
2021-01-19 14:18:51 -0500

    updateParticle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+
 import './App.css';
 import About from './Component/About/About';
 import Portfolio from './Component/Portfolio/Portfolio';
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="circle" bg={true} />
       <About/>
       <Portfolio/>
     </div>
